0c3d21a6fb8b52712106e20b43f847633086c17f
Yakshit Jain <jainyakshit1@gmail.com>
2024-07-26 15:29:51 +0530

    updated changes - 2024-07-26 15:29:51

--- Problem10_SimilarFilms.sql
+++ Problem10_SimilarFilms.sql
@@ -1,9 +1,2 @@
 -- Write a query to find films that have the same rental duration and replacement cost as the film "ACADEMY DINOSAUR". Use a multiple-column subquery.
 -- Write Query Here
-SELECT title, rental_duration, replacement_cost
-FROM film
-WHERE (rental_duration, replacement_cost) = (
-    SELECT rental_duration, replacement_cost
-    FROM film
-    WHERE title = 'ACADEMY DINOSAUR'
-);
--- Problem5_MostFrequentlyRentedMoviesWithInventory.sql
+++ Problem5_MostFrequentlyRentedMoviesWithInventory.sql
@@ -1,8 +1,2 @@
 -- Display the movies that have been rented the most frequently, considering only those films that are currently in the inventory, ordered by rental frequency in descending order.
 -- Write Query Here
-SELECT f.title, COUNT(r.rental_id) AS rental_count
-FROM film f
-INNER JOIN inventory i ON f.film_id = i.film_id
-INNER JOIN rental r ON i.inventory_id = r.inventory_id
-GROUP BY f.title
-ORDER BY rental_count DESC;
