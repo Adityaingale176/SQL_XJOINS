30480932db18063925ebf4387024bd556a2f05d2
CrioUser <crio-user@criodo.com>
2024-09-09 00:24:30 +0530

    ME_SQL_XJOINS_MODULE_EXERCISES

--- Problem5_MostFrequentlyRentedMoviesWithInventory.sql
+++ Problem5_MostFrequentlyRentedMoviesWithInventory.sql
@@ -1,7 +1,7 @@
 -- Display the movies that have been rented the most frequently, considering only those films that are currently in the inventory, ordered by rental frequency in descending order.
 -- Write Query Here
 
-SELECT title , COUNT(rental.rental_id) from film 
+SELECT title , COUNT(rental.rental_id) AS rental_count FROM film 
 JOIN inventory ON film.film_id = inventory.film_id
 JOIN rental ON inventory.inventory_id = rental.inventory_id
 GROUP BY film.film_id, title
